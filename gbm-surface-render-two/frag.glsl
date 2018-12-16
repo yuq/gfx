3f32cc9190d372026cf2b2a1eae633acefc467db
Qiang Yu <yuq825@gmail.com>
2018-12-16 16:13:41 +0800

    gbm-surface-render-two

---
+++ gbm-surface-render-two/frag.glsl
@@ -0,0 +1,89 @@
+#ifdef GL_ES
+precision mediump float;
+#endif
+#define RepeatNone               	      0
+#define RepeatNormal                     1
+#define RepeatPad                        2
+#define RepeatReflect                    3
+#define RepeatFix		      	      10
+uniform int 			source_repeat_mode;
+uniform int 			mask_repeat_mode;
+vec2 rel_tex_coord(vec2 texture, vec4 wh, int repeat) 
+{
+	vec2 rel_tex; 
+	rel_tex = texture * wh.xy; 
+	if (repeat == RepeatFix + RepeatNone)
+		return rel_tex; 
+	else if (repeat == RepeatFix + RepeatNormal) 
+		rel_tex = floor(rel_tex) + (fract(rel_tex) / wh.xy); 
+	else if (repeat == RepeatFix + RepeatPad) { 
+		if (rel_tex.x >= 1.0) 
+			rel_tex.x = 1.0 - wh.z * wh.x / 2.; 
+		else if (rel_tex.x < 0.0) 
+			rel_tex.x = 0.0; 
+		if (rel_tex.y >= 1.0) 
+			rel_tex.y = 1.0 - wh.w * wh.y / 2.; 
+		else if (rel_tex.y < 0.0) 
+			rel_tex.y = 0.0; 
+		rel_tex = rel_tex / wh.xy; 
+	} else if (repeat == RepeatFix + RepeatReflect) {
+		if ((1.0 - mod(abs(floor(rel_tex.x)), 2.0)) < 0.001)
+			rel_tex.x = 2.0 - (1.0 - fract(rel_tex.x)) / wh.x;
+		else 
+			rel_tex.x = fract(rel_tex.x) / wh.x;
+		if ((1.0 - mod(abs(floor(rel_tex.y)), 2.0)) < 0.001)
+			rel_tex.y = 2.0 - (1.0 - fract(rel_tex.y)) / wh.y;
+		else 
+			rel_tex.y = fract(rel_tex.y) / wh.y;
+	} 
+	return rel_tex; 
+}
+ vec4 rel_sampler_rgba(sampler2D tex_image, vec2 tex, vec4 wh, int repeat)
+{
+	if (repeat >= RepeatFix) {
+		tex = rel_tex_coord(tex, wh, repeat);
+		if (repeat == RepeatFix + RepeatNone) {
+			if (tex.x < 0.0 || tex.x >= 1.0 || 
+			    tex.y < 0.0 || tex.y >= 1.0)
+				return vec4(0.0, 0.0, 0.0, 0.0);
+			tex = (fract(tex) / wh.xy);
+		}
+	}
+	return texture2D(tex_image, tex);
+}
+ vec4 rel_sampler_rgbx(sampler2D tex_image, vec2 tex, vec4 wh, int repeat)
+{
+	if (repeat >= RepeatFix) {
+		tex = rel_tex_coord(tex, wh, repeat);
+		if (repeat == RepeatFix + RepeatNone) {
+			if (tex.x < 0.0 || tex.x >= 1.0 || 
+			    tex.y < 0.0 || tex.y >= 1.0)
+				return vec4(0.0, 0.0, 0.0, 0.0);
+			tex = (fract(tex) / wh.xy);
+		}
+	}
+	return vec4(texture2D(tex_image, tex).rgb, 1.0);
+}
+varying vec2 source_texture;
+uniform sampler2D source_sampler;
+uniform vec4 source_wh;vec4 get_source()
+{
+	return rel_sampler_rgba(source_sampler, source_texture,
+			        source_wh, source_repeat_mode);
+}
+varying vec2 mask_texture;
+uniform sampler2D mask_sampler;
+uniform vec4 mask_wh;
+vec4 get_mask()
+{
+	return rel_sampler_rgba(mask_sampler, mask_texture,
+			        mask_wh, mask_repeat_mode);
+}
+vec4 dest_swizzle(vec4 color)
+{	return color;}
+
+void main()
+{
+	gl_FragColor = dest_swizzle(get_source() * get_mask().a);
+}
+
---
+++ gbm-surface-render-two/main.c
@@ -0,0 +1,418 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+#include <fcntl.h>
+#include <unistd.h>
+
+#include <gbm.h>
+#include <png.h>
+
+#include <epoxy/gl.h>
+#include <epoxy/egl.h>
+
+GLuint program;
+EGLDisplay display;
+EGLSurface surface;
+EGLContext context;
+struct gbm_device *gbm;
+struct gbm_surface *gs;
+
+#define TARGET_SIZE 256
+
+EGLConfig get_config(void)
+{
+	EGLint egl_config_attribs[] = {
+		EGL_BUFFER_SIZE,	32,
+		EGL_DEPTH_SIZE,		EGL_DONT_CARE,
+		EGL_STENCIL_SIZE,	EGL_DONT_CARE,
+		EGL_RENDERABLE_TYPE,	EGL_OPENGL_ES2_BIT,
+		EGL_SURFACE_TYPE,	EGL_WINDOW_BIT,
+		EGL_NONE,
+	};
+
+	EGLint num_configs;
+	assert(eglGetConfigs(display, NULL, 0, &num_configs) == EGL_TRUE);
+
+	EGLConfig *configs = malloc(num_configs * sizeof(EGLConfig));
+	assert(eglChooseConfig(display, egl_config_attribs,
+			       configs, num_configs, &num_configs) == EGL_TRUE);
+	assert(num_configs);
+	printf("num config %d\n", num_configs);
+
+	// Find a config whose native visual ID is the desired GBM format.
+	for (int i = 0; i < num_configs; ++i) {
+		EGLint gbm_format;
+
+		assert(eglGetConfigAttrib(display, configs[i],
+					  EGL_NATIVE_VISUAL_ID, &gbm_format) == EGL_TRUE);
+		printf("gbm format %x\n", gbm_format);
+
+		if (gbm_format == GBM_FORMAT_ARGB8888) {
+			EGLConfig ret = configs[i];
+			free(configs);
+			return ret;
+		}
+	}
+
+	// Failed to find a config with matching GBM format.
+	abort();
+}
+
+void RenderTargetInit(void)
+{
+	assert(epoxy_has_egl_extension(EGL_NO_DISPLAY, "EGL_MESA_platform_gbm"));
+
+	int fd = open("/dev/dri/renderD128", O_RDWR);
+	assert(fd >= 0);
+
+	gbm = gbm_create_device(fd);
+	assert(gbm != NULL);
+
+	assert((display = eglGetPlatformDisplayEXT(EGL_PLATFORM_GBM_MESA, gbm, NULL)) != EGL_NO_DISPLAY);
+
+	EGLint majorVersion;
+	EGLint minorVersion;
+	assert(eglInitialize(display, &majorVersion, &minorVersion) == EGL_TRUE);
+
+	assert(eglBindAPI(EGL_OPENGL_ES_API) == EGL_TRUE);
+
+	EGLConfig config = get_config();
+
+	gs = gbm_surface_create(
+		gbm, TARGET_SIZE, TARGET_SIZE, GBM_BO_FORMAT_ARGB8888,
+		GBM_BO_USE_LINEAR|GBM_BO_USE_SCANOUT|GBM_BO_USE_RENDERING);
+	assert(gs);
+
+	assert((surface = eglCreatePlatformWindowSurfaceEXT(display, config, gs, NULL)) != EGL_NO_SURFACE);
+
+	const EGLint contextAttribs[] = {
+		EGL_CONTEXT_CLIENT_VERSION, 2,
+		EGL_NONE
+	};
+	assert((context = eglCreateContext(display, config, EGL_NO_CONTEXT, contextAttribs)) != EGL_NO_CONTEXT);
+
+	assert(eglMakeCurrent(display, surface, surface, context) == EGL_TRUE);
+}
+
+GLuint LoadShader(const char *name, GLenum type)
+{
+	FILE *f;
+	int size;
+	char *buff;
+	GLuint shader;
+	GLint compiled;
+	const GLchar *source[1];
+
+	assert((f = fopen(name, "r")) != NULL);
+
+	// get file size
+	fseek(f, 0, SEEK_END);
+	size = ftell(f);
+	fseek(f, 0, SEEK_SET);
+
+	assert((buff = malloc(size)) != NULL);
+	assert(fread(buff, 1, size, f) == size);
+	source[0] = buff;
+	fclose(f);
+	shader = glCreateShader(type);
+	glShaderSource(shader, 1, source, &size);
+	glCompileShader(shader);
+	free(buff);
+	glGetShaderiv(shader, GL_COMPILE_STATUS, &compiled);
+	if (!compiled) {
+		GLint infoLen = 0;
+		glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &infoLen);
+		if (infoLen > 1) {
+			char *infoLog = malloc(infoLen);
+			glGetShaderInfoLog(shader, infoLen, NULL, infoLog);
+			fprintf(stderr, "Error compiling shader %s:\n%s\n", name, infoLog);
+			free(infoLog);
+		}
+		glDeleteShader(shader);
+		return 0;
+	}
+
+	return shader;
+}
+
+void InitGLES(void)
+{
+	GLint linked;
+	GLuint vertexShader;
+	GLuint fragmentShader;
+	assert((vertexShader = LoadShader("vert.glsl", GL_VERTEX_SHADER)) != 0);
+	assert((fragmentShader = LoadShader("frag.glsl", GL_FRAGMENT_SHADER)) != 0);
+	assert((program = glCreateProgram()) != 0);
+	glAttachShader(program, vertexShader);
+	glAttachShader(program, fragmentShader);
+	glLinkProgram(program);
+	glGetProgramiv(program, GL_LINK_STATUS, &linked);
+	if (!linked) {
+		GLint infoLen = 0;
+		glGetProgramiv(program, GL_INFO_LOG_LENGTH, &infoLen);
+		if (infoLen > 1) {
+			char *infoLog = malloc(infoLen);
+			glGetProgramInfoLog(program, infoLen, NULL, infoLog);
+			fprintf(stderr, "Error linking program:\n%s\n", infoLog);
+			free(infoLog);
+		}
+		glDeleteProgram(program);
+		exit(1);
+	}
+
+	glClearColor(0, 0, 0, 0);
+	glViewport(0, 0, TARGET_SIZE, TARGET_SIZE);
+	//glEnable(GL_DEPTH_TEST);
+
+	glUseProgram(program);
+}
+
+void *readImage(char *filename, int *width, int *height)
+{
+	char header[8];    // 8 is the maximum size that can be checked
+
+        /* open file and test for it being a png */
+        FILE *fp = fopen(filename, "rb");
+	assert(fp);
+        fread(header, 1, 8, fp);
+        assert(!png_sig_cmp(header, 0, 8));
+
+        /* initialize stuff */
+        png_structp png_ptr = png_create_read_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
+	assert(png_ptr);
+
+        png_infop info_ptr = png_create_info_struct(png_ptr);
+	assert(info_ptr);
+
+        assert(!setjmp(png_jmpbuf(png_ptr)));
+
+        png_init_io(png_ptr, fp);
+        png_set_sig_bytes(png_ptr, 8);
+
+        png_read_info(png_ptr, info_ptr);
+
+        *width = png_get_image_width(png_ptr, info_ptr);
+        *height = png_get_image_height(png_ptr, info_ptr);
+        int color_type = png_get_color_type(png_ptr, info_ptr);
+	assert(color_type == PNG_COLOR_TYPE_RGB);
+        int bit_depth = png_get_bit_depth(png_ptr, info_ptr);
+	assert(bit_depth == 8);
+	int pitch = png_get_rowbytes(png_ptr, info_ptr);
+
+        int number_of_passes = png_set_interlace_handling(png_ptr);
+        png_read_update_info(png_ptr, info_ptr);
+
+        /* read file */
+        assert(!setjmp(png_jmpbuf(png_ptr)));
+
+	png_bytep buffer = malloc(*height * pitch);
+	void *ret = buffer;
+	assert(buffer);
+        png_bytep *row_pointers = malloc(sizeof(png_bytep) * *height);
+	assert(row_pointers);
+        for (int i = 0; i < *height; i++) {
+                row_pointers[i] = buffer;
+		buffer += pitch;
+	}
+
+        png_read_image(png_ptr, row_pointers);
+
+        fclose(fp);
+	free(row_pointers);
+	return ret;
+}
+
+int writeImage(char* filename, int width, int height, void *buffer, char* title)
+{
+	int code = 0;
+	FILE *fp = NULL;
+	png_structp png_ptr = NULL;
+	png_infop info_ptr = NULL;
+
+	// Open file for writing (binary mode)
+	fp = fopen(filename, "wb");
+	if (fp == NULL) {
+		fprintf(stderr, "Could not open file %s for writing\n", filename);
+		code = 1;
+		goto finalise;
+	}
+
+	// Initialize write structure
+	png_ptr = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
+	if (png_ptr == NULL) {
+		fprintf(stderr, "Could not allocate write struct\n");
+		code = 1;
+		goto finalise;
+	}
+
+	// Initialize info structure
+	info_ptr = png_create_info_struct(png_ptr);
+	if (info_ptr == NULL) {
+		fprintf(stderr, "Could not allocate info struct\n");
+		code = 1;
+		goto finalise;
+	}
+
+	// Setup Exception handling
+	if (setjmp(png_jmpbuf(png_ptr))) {
+		fprintf(stderr, "Error during png creation\n");
+		code = 1;
+		goto finalise;
+	}
+
+	png_init_io(png_ptr, fp);
+
+	// Write header (8 bit colour depth)
+	png_set_IHDR(png_ptr, info_ptr, width, height,
+		     8, PNG_COLOR_TYPE_RGB_ALPHA, PNG_INTERLACE_NONE,
+		     PNG_COMPRESSION_TYPE_DEFAULT, PNG_FILTER_TYPE_DEFAULT);
+
+	// Set title
+	if (title != NULL) {
+		png_text title_text;
+		title_text.compression = PNG_TEXT_COMPRESSION_NONE;
+		title_text.key = "Title";
+		title_text.text = title;
+		png_set_text(png_ptr, info_ptr, &title_text, 1);
+	}
+
+	png_write_info(png_ptr, info_ptr);
+
+	// Write image data
+	int i;
+	for (i = 0; i < height; i++)
+		png_write_row(png_ptr, (png_bytep)buffer + i * width * 4);
+
+	// End write
+	png_write_end(png_ptr, NULL);
+
+finalise:
+	if (fp != NULL) fclose(fp);
+	if (info_ptr != NULL) png_free_data(png_ptr, info_ptr, PNG_FREE_ALL, -1);
+	if (png_ptr != NULL) png_destroy_write_struct(&png_ptr, (png_infopp)NULL);
+
+	return code;
+}
+
+void Render(void)
+{
+	GLfloat vertex[] = {
+		-1, -1, 0,
+		-1, 1, 0,
+		1, 1, 0,
+		1, -1, 0
+	};
+
+	GLfloat texpos[] = {
+		0, 1,
+		0, 0,
+		1, 0,
+		1, 1,
+	};
+
+	GLint position = glGetAttribLocation(program, "v_position");
+	glEnableVertexAttribArray(position);
+	glVertexAttribPointer(position, 3, GL_FLOAT, 0, 0, vertex);
+
+	GLint tex0 = glGetAttribLocation(program, "v_texcoord0");
+	glEnableVertexAttribArray(tex0);
+	glVertexAttribPointer(tex0, 2, GL_FLOAT, 0, 0, texpos);
+
+	GLint tex1 = glGetAttribLocation(program, "v_texcoord1");
+	glEnableVertexAttribArray(tex1);
+	glVertexAttribPointer(tex1, 2, GL_FLOAT, 0, 0, texpos);
+
+	GLint mask_repeat_mode = glGetUniformLocation(program, "mask_repeat_mode");
+	glUniform1i(mask_repeat_mode, 0);
+
+	GLint mask_sampler = glGetUniformLocation(program, "mask_sampler");
+	glUniform1i(mask_sampler, 0);
+
+	GLint mask_wh = glGetUniformLocation(program, "mask_wh");
+	glUniform4f(mask_wh, 1.0, 1.0, 1.0/TARGET_SIZE, 1.0/TARGET_SIZE);
+
+	glActiveTexture(GL_TEXTURE0);
+
+	uint8_t *data = malloc(TARGET_SIZE * TARGET_SIZE);
+	assert(data);
+	for (int i = 0; i < TARGET_SIZE; i++) {
+		int a = (i - TARGET_SIZE/2);
+		a *= a;
+		
+		for (int j = 0; j < TARGET_SIZE; j++) {
+			int b = (j - TARGET_SIZE/2);
+			b *= b;
+
+			if (a + b < TARGET_SIZE * TARGET_SIZE / 8)
+				data[i * TARGET_SIZE + j] = 0xff;
+			else
+				data[i * TARGET_SIZE + j] = 0;
+		}
+	}
+	
+	GLuint texid = 0;
+        glGenTextures(1, &texid);
+	glBindTexture(GL_TEXTURE_2D, texid);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_ALPHA, TARGET_SIZE, TARGET_SIZE, 0, GL_ALPHA,
+		     GL_UNSIGNED_BYTE, data);
+
+	int w, h;
+	data = readImage("crate-base.png", &w, &h);
+
+	GLint source_repeat_mode = glGetUniformLocation(program, "source_repeat_mode");
+	glUniform1i(source_repeat_mode, 0);
+
+	GLint source_sampler = glGetUniformLocation(program, "source_sampler");
+	glUniform1i(source_sampler, 1);
+
+	GLint source_wh = glGetUniformLocation(program, "source_wh");
+	glUniform4f(source_wh, 1, 1, 1.0/w, 1.0/h);
+
+	glActiveTexture(GL_TEXTURE1);
+	glGenTextures(1, &texid);
+	glBindTexture(GL_TEXTURE_2D, texid);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, w, h, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	glDrawArrays(GL_TRIANGLES, 0, 3);
+
+	assert(glGetError() == GL_NO_ERROR);
+
+	eglSwapBuffers(display, surface);
+
+#if 1
+	GLubyte result[TARGET_SIZE * TARGET_SIZE * 4] = {0};
+	glReadPixels(0, 0, TARGET_SIZE, TARGET_SIZE, GL_RGBA, GL_UNSIGNED_BYTE, result);
+	assert(glGetError() == GL_NO_ERROR);
+#else
+	struct gbm_bo *bo = gbm_surface_lock_front_buffer(gs);
+	assert(bo);
+
+	uint32_t stride;
+	void *map_data;
+	GLubyte *result = gbm_bo_map(bo, 0, 0, TARGET_SIZE, TARGET_SIZE,
+				     GBM_BO_TRANSFER_READ, &stride, &map_data);
+	assert(result);
+	assert(stride == TARGET_SIZE * 4);
+#endif
+
+	assert(!writeImage("screenshot.png", TARGET_SIZE, TARGET_SIZE, result, "hello"));
+}
+
+int main(void)
+{
+	RenderTargetInit();
+	InitGLES();
+	Render();
+	return 0;
+}

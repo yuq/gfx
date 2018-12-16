5cde4e734d4fa03771d6c6d1350a8fcd25d97923
Qiang Yu <yuq825@gmail.com>
2018-12-16 16:13:10 +0800

    add gbm-surface-render

---
+++ gbm-surface-render/vert.glsl
@@ -0,0 +1,10 @@
+attribute vec4 v_position;
+attribute vec4 v_texcoord0;
+attribute vec4 v_texcoord1;
+varying vec2 source_texture;
+varying vec2 mask_texture;
+void main()
+{
+	gl_Position = v_position;
+	mask_texture = v_texcoord1.xy;
+}

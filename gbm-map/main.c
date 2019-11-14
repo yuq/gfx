c058e1b71cf7f294f623c444d067afbcee39096f
Qiang Yu <yuq825@gmail.com>
2019-11-14 17:36:29 +0800

    gbm-map: repeat map/unamp for testing staging buffer

--- gbm-map/main.c
+++ gbm-map/main.c
@@ -9,26 +9,31 @@
 
 int main(void)
 {
-        int fd = open("/dev/dri/card0", O_RDWR);
-	//int fd = open("/dev/dri/renderD128", O_RDWR);
+	//int fd = open("/dev/dri/card0", O_RDWR);
+	int fd = open("/dev/dri/renderD128", O_RDWR);
 	assert(fd >= 0);
 
 	struct gbm_device *gbm = gbm_create_device(fd);
 	assert(gbm != NULL);
 
 	struct gbm_bo *bo = gbm_bo_create(
-		gbm, 1920, 1080, GBM_FORMAT_ARGB8888,
-		GBM_BO_USE_RENDERING|GBM_BO_USE_SCANOUT|GBM_BO_USE_LINEAR);
+		gbm, 1280, 720, GBM_FORMAT_ARGB8888,
+		GBM_BO_USE_RENDERING|GBM_BO_USE_LINEAR);
 	assert(bo);
 
-	uint32_t stride;
-	void *data;
-	void *map = gbm_bo_map(bo, 0, 0, 1920, 1080, GBM_BO_TRANSFER_READ_WRITE, &stride, &data);
-	assert(map);
+	for (int i = 0; i < 100; i++) {
+		uint32_t stride = 0;
+		void *data = NULL;
+		void *map = gbm_bo_map(bo, 0, 0, 1280, 720, GBM_BO_TRANSFER_READ, &stride, &data);
+		assert(map);
 
-	printf("map=%x %p\n", *(unsigned *)map, map);
+		usleep(50000);
+		printf("i=%d\n", i);
 	
-	gbm_bo_unmap(bo, data);
+		gbm_bo_unmap(bo, data);
+
+		usleep(50000);
+	}
 	
 	return 0;
 }

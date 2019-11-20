a5a9c7c0a342093d8a21f8b5088ad8ad28ef3dda
Qiang Yu <yuq825@gmail.com>
2019-11-20 19:35:26 +0800

    add rgb2yuv/image

---
+++ rgb2yuv/image/comp.glsl
@@ -0,0 +1,45 @@
+#version 430
+layout(local_size_x = 16, local_size_y = 16) in;
+layout(rgba8, binding = 0) uniform readonly mediump image2D img_in;
+layout(r8, binding = 1) uniform writeonly mediump image2D img_out;
+
+void main()
+{
+	ivec2 idx0 = ivec2(gl_GlobalInvocationID.xy) * 2;
+	ivec2 idx1 = ivec2(idx0.x + 1, idx0.y);
+	ivec2 idx2 = ivec2(idx0.x, idx0.y + 1);
+	ivec2 idx3 = ivec2(idx0.x + 1, idx0.y + 1);
+	ivec2 base = imageSize(img_in);
+
+	if (any(greaterThanEqual(idx3, base)))
+	    return;
+
+	vec3 rgb0 = imageLoad(img_in, idx0).rgb;
+	vec3 rgb1 = imageLoad(img_in, idx1).rgb;
+	vec3 rgb2 = imageLoad(img_in, idx2).rgb;
+	vec3 rgb3 = imageLoad(img_in, idx3).rgb;
+
+	mat3 conv_mat = mat3(
+	     0.299,    0.587,    0.114,
+	     -0.14713, -0.28886, 0.436,
+	     0.615,    -0.51499, -0.10001
+	);
+
+	vec3 yuv0 = conv_mat * rgb0;
+	vec3 yuv1 = conv_mat * rgb1;
+	vec3 yuv2 = conv_mat * rgb2;
+	vec3 yuv3 = conv_mat * rgb3;
+
+	imageStore(img_out, idx0, vec4(yuv0.r));
+	imageStore(img_out, idx1, vec4(yuv1.r));
+	imageStore(img_out, idx2, vec4(yuv2.r));
+	imageStore(img_out, idx3, vec4(yuv3.r));
+
+	ivec2 uidx = ivec2(
+	      int(gl_GlobalInvocationID.x) * 2,
+	      base.y + int(gl_GlobalInvocationID.y));
+	ivec2 vidx = ivec2(uidx.x + 1, uidx.y);
+	vec2 uv = (yuv0.gb + yuv1.gb + yuv2.gb + yuv3.gb) / 4.0;
+	imageStore(img_out, uidx, vec4(uv.x));
+	imageStore(img_out, vidx, vec4(uv.y));
+}

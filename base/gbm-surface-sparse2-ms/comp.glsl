1a082d9487328b5265280076815b3b29e6aea22d
Qiang Yu <yuq825@gmail.com>
2021-12-31 14:38:01 +0800

    add base/gbm-surface-sparse2-ms

---
+++ base/gbm-surface-sparse2-ms/comp.glsl
@@ -0,0 +1,13 @@
+#version 430 compatibility
+#extension GL_ARB_sparse_texture2: enable
+layout(local_size_x = 16, local_size_y = 16) in;
+layout(rgba8, binding = 0) uniform writeonly mediump image2DMS img_out;
+
+void main()
+{
+	ivec2 pos = ivec2(gl_GlobalInvocationID.xy);
+	imageStore(img_out, pos, 0, vec4(1.0, 0.0, 0.0, 1.0));
+	imageStore(img_out, pos, 1, vec4(0.0, 1.0, 0.0, 1.0));
+	imageStore(img_out, pos, 2, vec4(0.0, 0.0, 1.0, 1.0));
+	imageStore(img_out, pos, 3, vec4(1.0, 1.0, 0.0, 1.0));
+}
---
+++ base/gbm-surface-sparse2-ms/main.c
@@ -0,0 +1,389 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+#include <fcntl.h>
+#include <unistd.h>
+
+#include <gbm.h>
+#include <png.h>
+
+#include <epoxy/gl.h>
+#include <epoxy/egl.h>
+
+GLuint program;
+EGLDisplay display;
+EGLSurface surface;
+EGLContext context;
+struct gbm_device *gbm;
+struct gbm_surface *gs;
+
+#define TARGET_SIZE 256
+
+EGLConfig get_config(void)
+{
+	EGLint egl_config_attribs[] = {
+		EGL_BUFFER_SIZE,	32,
+		EGL_DEPTH_SIZE,		EGL_DONT_CARE,
+		EGL_STENCIL_SIZE,	EGL_DONT_CARE,
+		EGL_RENDERABLE_TYPE,	EGL_OPENGL_ES2_BIT,
+		EGL_SURFACE_TYPE,	EGL_WINDOW_BIT,
+		EGL_NONE,
+	};
+
+	EGLint num_configs;
+	assert(eglGetConfigs(display, NULL, 0, &num_configs) == EGL_TRUE);
+
+	EGLConfig *configs = malloc(num_configs * sizeof(EGLConfig));
+	assert(eglChooseConfig(display, egl_config_attribs,
+			       configs, num_configs, &num_configs) == EGL_TRUE);
+	assert(num_configs);
+	printf("num config %d\n", num_configs);
+
+	// Find a config whose native visual ID is the desired GBM format.
+	for (int i = 0; i < num_configs; ++i) {
+		EGLint gbm_format;
+
+		assert(eglGetConfigAttrib(display, configs[i],
+					  EGL_NATIVE_VISUAL_ID, &gbm_format) == EGL_TRUE);
+		printf("gbm format %x\n", gbm_format);
+
+		if (gbm_format == GBM_FORMAT_ARGB8888) {
+			EGLConfig ret = configs[i];
+			free(configs);
+			return ret;
+		}
+	}
+
+	// Failed to find a config with matching GBM format.
+	abort();
+}
+
+void RenderTargetInit(void)
+{
+	assert(epoxy_has_egl_extension(EGL_NO_DISPLAY, "EGL_MESA_platform_gbm"));
+
+	int fd = open("/dev/dri/renderD128", O_RDWR);
+	assert(fd >= 0);
+
+	gbm = gbm_create_device(fd);
+	assert(gbm != NULL);
+
+	assert((display = eglGetPlatformDisplayEXT(EGL_PLATFORM_GBM_MESA, gbm, NULL)) != EGL_NO_DISPLAY);
+
+	EGLint majorVersion;
+	EGLint minorVersion;
+	assert(eglInitialize(display, &majorVersion, &minorVersion) == EGL_TRUE);
+
+	assert(eglBindAPI(EGL_OPENGL_API) == EGL_TRUE);
+
+	EGLConfig config = get_config();
+
+	gs = gbm_surface_create(
+		gbm, TARGET_SIZE, TARGET_SIZE, GBM_BO_FORMAT_ARGB8888,
+		GBM_BO_USE_LINEAR|GBM_BO_USE_SCANOUT|GBM_BO_USE_RENDERING);
+	assert(gs);
+
+	assert((surface = eglCreatePlatformWindowSurfaceEXT(display, config, gs, NULL)) != EGL_NO_SURFACE);
+
+	assert((context = eglCreateContext(display, config, EGL_NO_CONTEXT, NULL)) != EGL_NO_CONTEXT);
+
+	assert(eglMakeCurrent(display, surface, surface, context) == EGL_TRUE);
+
+	printf("OpenGL version %s\n", glGetString(GL_VERSION));
+}
+
+GLuint LoadShader(const char *name, GLenum type)
+{
+	FILE *f;
+	int size;
+	char *buff;
+	GLuint shader;
+	GLint compiled;
+	const GLchar *source[1];
+
+	assert((f = fopen(name, "r")) != NULL);
+
+	// get file size
+	fseek(f, 0, SEEK_END);
+	size = ftell(f);
+	fseek(f, 0, SEEK_SET);
+
+	assert((buff = malloc(size)) != NULL);
+	assert(fread(buff, 1, size, f) == size);
+	source[0] = buff;
+	fclose(f);
+	shader = glCreateShader(type);
+	glShaderSource(shader, 1, source, &size);
+	glCompileShader(shader);
+	free(buff);
+	glGetShaderiv(shader, GL_COMPILE_STATUS, &compiled);
+	if (!compiled) {
+		GLint infoLen = 0;
+		glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &infoLen);
+		if (infoLen > 1) {
+			char *infoLog = malloc(infoLen);
+			glGetShaderInfoLog(shader, infoLen, NULL, infoLog);
+			fprintf(stderr, "Error compiling shader %s:\n%s\n", name, infoLog);
+			free(infoLog);
+		}
+		glDeleteShader(shader);
+		return 0;
+	}
+
+	return shader;
+}
+
+void compile(const char *name)
+{
+	GLint linked;
+        GLuint shader;
+	assert((shader = LoadShader(name, GL_COMPUTE_SHADER)) != 0);
+	assert((program = glCreateProgram()) != 0);
+	glAttachShader(program, shader);
+	glLinkProgram(program);
+	glGetProgramiv(program, GL_LINK_STATUS, &linked);
+	if (!linked) {
+		GLint infoLen = 0;
+		glGetProgramiv(program, GL_INFO_LOG_LENGTH, &infoLen);
+		if (infoLen > 1) {
+			char *infoLog = malloc(infoLen);
+			glGetProgramInfoLog(program, infoLen, NULL, infoLog);
+			fprintf(stderr, "Error linking program:\n%s\n", infoLog);
+			free(infoLog);
+		}
+		glDeleteProgram(program);
+		exit(1);
+	}
+
+	glUseProgram(program);
+}
+
+void *readImage(char *filename, int *width, int *height)
+{
+	char header[8];    // 8 is the maximum size that can be checked
+
+        /* open file and test for it being a png */
+        FILE *fp = fopen(filename, "rb");
+	assert(fp);
+        fread(header, 1, 8, fp);
+        assert(!png_sig_cmp(header, 0, 8));
+
+        /* initialize stuff */
+        png_structp png_ptr = png_create_read_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
+	assert(png_ptr);
+
+        png_infop info_ptr = png_create_info_struct(png_ptr);
+	assert(info_ptr);
+
+        assert(!setjmp(png_jmpbuf(png_ptr)));
+
+        png_init_io(png_ptr, fp);
+        png_set_sig_bytes(png_ptr, 8);
+
+        png_read_info(png_ptr, info_ptr);
+
+        *width = png_get_image_width(png_ptr, info_ptr);
+        *height = png_get_image_height(png_ptr, info_ptr);
+        int color_type = png_get_color_type(png_ptr, info_ptr);
+	assert(color_type == PNG_COLOR_TYPE_RGB_ALPHA);
+        int bit_depth = png_get_bit_depth(png_ptr, info_ptr);
+	assert(bit_depth == 8);
+	int pitch = png_get_rowbytes(png_ptr, info_ptr);
+
+        int number_of_passes = png_set_interlace_handling(png_ptr);
+        png_read_update_info(png_ptr, info_ptr);
+
+        /* read file */
+        assert(!setjmp(png_jmpbuf(png_ptr)));
+
+	png_bytep buffer = malloc(*height * pitch);
+	void *ret = buffer;
+	assert(buffer);
+        png_bytep *row_pointers = malloc(sizeof(png_bytep) * *height);
+	assert(row_pointers);
+        for (int i = 0; i < *height; i++) {
+                row_pointers[i] = buffer;
+		buffer += pitch;
+	}
+
+        png_read_image(png_ptr, row_pointers);
+
+        fclose(fp);
+	free(row_pointers);
+	return ret;
+}
+
+int writeImage(char* filename, int width, int height, void *buffer, char* title)
+{
+	int code = 0;
+	FILE *fp = NULL;
+	png_structp png_ptr = NULL;
+	png_infop info_ptr = NULL;
+
+	// Open file for writing (binary mode)
+	fp = fopen(filename, "wb");
+	if (fp == NULL) {
+		fprintf(stderr, "Could not open file %s for writing\n", filename);
+		code = 1;
+		goto finalise;
+	}
+
+	// Initialize write structure
+	png_ptr = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
+	if (png_ptr == NULL) {
+		fprintf(stderr, "Could not allocate write struct\n");
+		code = 1;
+		goto finalise;
+	}
+
+	// Initialize info structure
+	info_ptr = png_create_info_struct(png_ptr);
+	if (info_ptr == NULL) {
+		fprintf(stderr, "Could not allocate info struct\n");
+		code = 1;
+		goto finalise;
+	}
+
+	// Setup Exception handling
+	if (setjmp(png_jmpbuf(png_ptr))) {
+		fprintf(stderr, "Error during png creation\n");
+		code = 1;
+		goto finalise;
+	}
+
+	png_init_io(png_ptr, fp);
+
+	// Write header (8 bit colour depth)
+	png_set_IHDR(png_ptr, info_ptr, width, height,
+		     8, PNG_COLOR_TYPE_RGB_ALPHA, PNG_INTERLACE_NONE,
+		     PNG_COMPRESSION_TYPE_DEFAULT, PNG_FILTER_TYPE_DEFAULT);
+
+	// Set title
+	if (title != NULL) {
+		png_text title_text;
+		title_text.compression = PNG_TEXT_COMPRESSION_NONE;
+		title_text.key = "Title";
+		title_text.text = title;
+		png_set_text(png_ptr, info_ptr, &title_text, 1);
+	}
+
+	png_write_info(png_ptr, info_ptr);
+
+	// Write image data
+	int i;
+	for (i = 0; i < height; i++)
+		png_write_row(png_ptr, (png_bytep)buffer + i * width * 4);
+
+	// End write
+	png_write_end(png_ptr, NULL);
+
+finalise:
+	if (fp != NULL) fclose(fp);
+	if (info_ptr != NULL) png_free_data(png_ptr, info_ptr, PNG_FREE_ALL, -1);
+	if (png_ptr != NULL) png_destroy_write_struct(&png_ptr, (png_infopp)NULL);
+
+	return code;
+}
+
+void Render(void)
+{
+	assert(epoxy_has_gl_extension("GL_ARB_sparse_texture"));
+	assert(epoxy_has_gl_extension("GL_ARB_sparse_texture2"));
+
+	GLint value;
+	glGetIntegerv(GL_MAX_SPARSE_TEXTURE_SIZE_ARB, &value);
+	printf("GL_MAX_SPARSE_TEXTURE_SIZE_ARB=%x\n", value);
+	glGetIntegerv(GL_MAX_SPARSE_3D_TEXTURE_SIZE_ARB, &value);
+	printf("GL_MAX_SPARSE_3D_TEXTURE_SIZE_ARB=%x\n", value);
+	glGetIntegerv(GL_MAX_SPARSE_ARRAY_TEXTURE_LAYERS_ARB, &value);
+	printf("GL_MAX_SPARSE_ARRAY_TEXTURE_LAYERS_ARB=%x\n", value);
+	glGetIntegerv(GL_SPARSE_TEXTURE_FULL_ARRAY_CUBE_MIPMAPS_ARB, &value);
+	printf("GL_SPARSE_TEXTURE_FULL_ARRAY_CUBE_MIPMAPS_ARB=%x\n", value);
+
+	assert(glGetError() == GL_NO_ERROR);
+
+	glGetInternalformativ(GL_TEXTURE_2D_MULTISAMPLE, GL_RGBA8,
+			      GL_NUM_VIRTUAL_PAGE_SIZES_ARB,
+			      1, &value);
+	assert(value == 1);
+
+	glGetInternalformativ(GL_TEXTURE_2D_MULTISAMPLE, GL_RGBA8,
+			      GL_VIRTUAL_PAGE_SIZE_X_ARB,
+			      1, &value);
+	assert(value == 128);
+	glGetInternalformativ(GL_TEXTURE_2D_MULTISAMPLE, GL_RGBA8,
+			      GL_VIRTUAL_PAGE_SIZE_Y_ARB,
+			      1, &value);
+	assert(value == 128);
+	glGetInternalformativ(GL_TEXTURE_2D_MULTISAMPLE, GL_RGBA8,
+			      GL_VIRTUAL_PAGE_SIZE_Z_ARB,
+			      1, &value);
+	assert(value == 1);
+
+	assert(glGetError() == GL_NO_ERROR);
+
+	glActiveTexture(GL_TEXTURE0);
+
+	GLuint texin;
+        glGenTextures(1, &texin);
+	glBindTexture(GL_TEXTURE_2D_MULTISAMPLE, texin);
+	glTexParameteri(GL_TEXTURE_2D_MULTISAMPLE, GL_VIRTUAL_PAGE_SIZE_INDEX_ARB, 0);
+	glTexParameteri(GL_TEXTURE_2D_MULTISAMPLE, GL_TEXTURE_SPARSE_ARB, 1);
+
+	assert(glGetError() == GL_NO_ERROR);
+
+	glTexStorage2DMultisample(GL_TEXTURE_2D_MULTISAMPLE, 4, GL_RGBA8,
+				  TARGET_SIZE, TARGET_SIZE, true);
+
+	assert(glGetError() == GL_NO_ERROR);
+
+	glGetTexParameteriv(GL_TEXTURE_2D_MULTISAMPLE, GL_NUM_SPARSE_LEVELS_ARB, &value);
+	printf("GL_NUM_SPARSE_LEVELS_ARB=%x\n", value);
+
+	assert(glGetError() == GL_NO_ERROR);
+
+	glTexPageCommitmentARB(GL_TEXTURE_2D_MULTISAMPLE, 0, 0, 0, 0, 128, 128, 1, true);
+
+	assert(glGetError() == GL_NO_ERROR);
+
+	glBindImageTexture(0, texin, 0, GL_FALSE, 0, GL_WRITE_ONLY, GL_RGBA8);
+
+	assert(glGetError() == GL_NO_ERROR);
+
+	compile("comp.glsl");
+
+	glDispatchCompute(TARGET_SIZE >> 4, TARGET_SIZE >> 4, 1);
+	glMemoryBarrier(GL_SHADER_IMAGE_ACCESS_BARRIER_BIT);
+
+	compile("copy.glsl");
+
+	glBindImageTexture(0, texin, 0, GL_FALSE, 0, GL_READ_ONLY, GL_RGBA8);
+
+	GLuint texout;
+        glGenTextures(1, &texout);
+	glBindTexture(GL_TEXTURE_2D, texout);
+	glTextureStorage2D(texout, 1, GL_RGBA8, TARGET_SIZE, TARGET_SIZE);	
+	glBindImageTexture(1, texout, 0, GL_FALSE, 0, GL_WRITE_ONLY, GL_RGBA8);
+
+	assert(glGetError() == GL_NO_ERROR);
+
+	glDispatchCompute(TARGET_SIZE >> 4, TARGET_SIZE >> 4, 1);
+	glMemoryBarrier(GL_SHADER_IMAGE_ACCESS_BARRIER_BIT);
+
+	assert(glGetError() == GL_NO_ERROR);
+
+	GLubyte *result = calloc(1, TARGET_SIZE * TARGET_SIZE * 4);
+	glGetTextureImage(texout, 0, GL_RGBA, GL_UNSIGNED_BYTE,
+			  TARGET_SIZE * TARGET_SIZE * 4, result);
+	assert(glGetError() == GL_NO_ERROR);
+
+	assert(!writeImage("screenshot.png", TARGET_SIZE, TARGET_SIZE, result, "hello"));
+}
+
+int main(void)
+{
+	RenderTargetInit();
+	Render();
+	return 0;
+}

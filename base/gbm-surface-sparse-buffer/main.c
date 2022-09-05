8e6eccfac5409fb4ff3a3650fc350c6f97950778
Qiang Yu <yuq825@gmail.com>
2022-09-05 15:50:07 +0800

    add gbm-surface-sparse-buffer

---
+++ base/gbm-surface-sparse-buffer/main.c
@@ -0,0 +1,144 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+#include <fcntl.h>
+#include <unistd.h>
+
+#include <gbm.h>
+
+#include <epoxy/gl.h>
+#include <epoxy/egl.h>
+
+EGLDisplay display;
+EGLContext context;
+struct gbm_device *gbm;
+
+void init_context(void)
+{
+	assert(epoxy_has_egl_extension(EGL_NO_DISPLAY, "EGL_MESA_platform_gbm"));
+
+	int fd = open("/dev/dri/renderD128", O_RDWR);
+	assert(fd >= 0);
+
+	gbm = gbm_create_device(fd);
+	assert(gbm != NULL);
+
+	assert((display = eglGetPlatformDisplayEXT(EGL_PLATFORM_GBM_MESA, gbm, NULL)) != EGL_NO_DISPLAY);
+
+	EGLint majorVersion;
+	EGLint minorVersion;
+	assert(eglInitialize(display, &majorVersion, &minorVersion) == EGL_TRUE);
+	assert(epoxy_has_egl_extension(display, "EGL_KHR_surfaceless_context"));
+
+	assert(eglBindAPI(EGL_OPENGL_API) == EGL_TRUE);
+
+	const EGLint contextAttribs[] = {
+		EGL_CONTEXT_MAJOR_VERSION, 4,
+		EGL_CONTEXT_MINOR_VERSION, 5,
+		EGL_NONE
+	};
+	assert((context = eglCreateContext(display, NULL, EGL_NO_CONTEXT, contextAttribs)) != EGL_NO_CONTEXT);
+
+	assert(eglMakeCurrent(display, EGL_NO_SURFACE, EGL_NO_SURFACE, context) == EGL_TRUE);
+}
+
+GLuint LoadShader(const char *name, GLenum type)
+{
+	FILE *f;
+	int size;
+	char *buff;
+	GLuint shader;
+	GLint compiled;
+	const GLchar *source[1];
+
+	assert((f = fopen(name, "r")) != NULL);
+
+	// get file size
+	fseek(f, 0, SEEK_END);
+	size = ftell(f);
+	fseek(f, 0, SEEK_SET);
+
+	assert((buff = malloc(size)) != NULL);
+	assert(fread(buff, 1, size, f) == size);
+	source[0] = buff;
+	fclose(f);
+	shader = glCreateShader(type);
+	glShaderSource(shader, 1, source, &size);
+	glCompileShader(shader);
+	free(buff);
+	glGetShaderiv(shader, GL_COMPILE_STATUS, &compiled);
+	if (!compiled) {
+		GLint infoLen = 0;
+		glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &infoLen);
+		if (infoLen > 1) {
+			char *infoLog = malloc(infoLen);
+			glGetShaderInfoLog(shader, infoLen, NULL, infoLog);
+			fprintf(stderr, "Error compiling shader %s:\n%s\n", name, infoLog);
+			free(infoLog);
+		}
+		glDeleteShader(shader);
+		return 0;
+	}
+
+	return shader;
+}
+
+void init_gles(void)
+{
+	GLuint shader;
+	assert((shader = LoadShader("comp.glsl", GL_COMPUTE_SHADER)) != 0);
+
+	GLuint program;
+	assert((program = glCreateProgram()) != 0);
+	glAttachShader(program, shader);
+
+	GLint linked;
+	glLinkProgram(program);
+	glGetProgramiv(program, GL_LINK_STATUS, &linked);
+	if (!linked) {
+		GLint infoLen = 0;
+		glGetProgramiv(program, GL_INFO_LOG_LENGTH, &infoLen);
+		if (infoLen > 1) {
+			char *infoLog = malloc(infoLen);
+			glGetProgramInfoLog(program, infoLen, NULL, infoLog);
+			fprintf(stderr, "Error linking program:\n%s\n", infoLog);
+			free(infoLog);
+		}
+		glDeleteProgram(program);
+		exit(1);
+	}
+
+	glUseProgram(program);
+}
+
+void render(void)
+{
+	assert(epoxy_has_gl_extension("GL_ARB_sparse_buffer"));
+
+	uint64_t size = 4000000000;
+
+	GLint page;
+	glGetIntegerv(GL_SPARSE_BUFFER_PAGE_SIZE_ARB, &page);
+
+	GLuint buffer;
+	glGenBuffers(1, &buffer);
+	glBindBuffer(GL_UNIFORM_BUFFER, buffer);
+	glNamedBufferStorage(buffer, size, NULL,
+			     GL_DYNAMIC_STORAGE_BIT|GL_SPARSE_STORAGE_BIT_ARB);
+
+	assert(glGetError() == GL_NO_ERROR);
+
+	uint64_t page_mask = page - 1;
+	glNamedBufferPageCommitmentARB(buffer, size & ~page_mask, size & page_mask, true);
+
+	assert(glGetError() == GL_NO_ERROR);
+}
+
+int main(int argc, char **argv)
+{
+	init_context();
+	//init_gles();
+        render();
+	return 0;
+}

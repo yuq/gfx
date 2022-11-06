d418ac9e53ab7b4da11216b73f9205588b3bf98a
Qiang Yu <yuq825@gmail.com>
2022-11-06 14:17:51 +0800

    add vulkan/debug

---
+++ vulkan/debug/main.c
@@ -0,0 +1,591 @@
+#include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <png.h>
+
+#include <vulkan/vulkan.h>
+
+#define ENABLE_DEBUG_LAYERS
+
+#define TARGET_W 256
+#define TARGET_H 256
+
+static int writeImage(char* filename, int width, int height, int stride, void *buffer, char* title)
+{
+	int code = 0;
+	FILE *fp = NULL;
+	png_structp png_ptr = NULL;
+	png_infop info_ptr = NULL;
+
+	// Open file for writing (binary mode)
+	fp = fopen(filename, "wb");
+	if (fp == NULL) {
+		fprintf(stderr, "Could not open file %s for writing\n", filename);
+		code = 1;
+		goto finalise;
+	}
+
+	// Initialize write structure
+	png_ptr = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
+	if (png_ptr == NULL) {
+		fprintf(stderr, "Could not allocate write struct\n");
+		code = 1;
+		goto finalise;
+	}
+
+	// Initialize info structure
+	info_ptr = png_create_info_struct(png_ptr);
+	if (info_ptr == NULL) {
+		fprintf(stderr, "Could not allocate info struct\n");
+		code = 1;
+		goto finalise;
+	}
+
+	// Setup Exception handling
+	if (setjmp(png_jmpbuf(png_ptr))) {
+		fprintf(stderr, "Error during png creation\n");
+		code = 1;
+		goto finalise;
+	}
+
+	png_init_io(png_ptr, fp);
+
+	// Write header (8 bit colour depth)
+	png_set_IHDR(png_ptr, info_ptr, width, height,
+		     8, PNG_COLOR_TYPE_RGB_ALPHA, PNG_INTERLACE_NONE,
+		     PNG_COMPRESSION_TYPE_DEFAULT, PNG_FILTER_TYPE_DEFAULT);
+
+	// Set title
+	if (title != NULL) {
+		png_text title_text;
+		title_text.compression = PNG_TEXT_COMPRESSION_NONE;
+		title_text.key = "Title";
+		title_text.text = title;
+		png_set_text(png_ptr, info_ptr, &title_text, 1);
+	}
+
+	png_write_info(png_ptr, info_ptr);
+
+	// Write image data
+	int i;
+	for (i = 0; i < height; i++)
+		png_write_row(png_ptr, (png_bytep)buffer + i * stride);
+
+	// End write
+	png_write_end(png_ptr, NULL);
+
+finalise:
+	if (fp != NULL) fclose(fp);
+	if (info_ptr != NULL) png_free_data(png_ptr, info_ptr, PNG_FREE_ALL, -1);
+	if (png_ptr != NULL) png_destroy_write_struct(&png_ptr, (png_infopp)NULL);
+
+	return code;
+}
+
+static VkShaderModule createShaderModule(VkDevice device, const char *name)
+{
+	FILE *f;
+	int size;
+	void *code;
+
+	assert((f = fopen(name, "r")) != NULL);
+
+	// get file size
+	fseek(f, 0, SEEK_END);
+	size = ftell(f);
+	fseek(f, 0, SEEK_SET);
+
+	assert((code = malloc(size)) != NULL);
+	assert(fread(code, 1, size, f) == size);
+	fclose(f);
+
+	VkShaderModuleCreateInfo info = {
+		.sType = VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO,
+		.codeSize = size,
+		.pCode = code,
+	};
+	VkShaderModule shader;
+        assert(vkCreateShaderModule(device, &info, NULL, &shader) == VK_SUCCESS);
+
+	free(code);
+
+	return shader;
+}
+
+#ifdef ENABLE_DEBUG_LAYERS
+static VkBool32 debug_callback(
+    VkDebugReportFlagsEXT       flags,
+    VkDebugReportObjectTypeEXT  objectType,
+    uint64_t                    object,
+    size_t                      location,
+    int32_t                     messageCode,
+    const char*                 pLayerPrefix,
+    const char*                 pMessage,
+    void*                       pUserData)
+{
+    printf("%s\n", pMessage);
+    return VK_FALSE;    // Since we don't want to fail the original call
+}
+#endif
+
+int main(void)
+{
+	VkInstance inst;
+	{
+		VkInstanceCreateInfo info = {
+			.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO,
+#ifdef ENABLE_DEBUG_LAYERS
+			.enabledLayerCount = 1,
+			.ppEnabledLayerNames = (const char *[]) {
+				"VK_LAYER_LUNARG_standard_validation",
+			},
+			.enabledExtensionCount = 1,
+			.ppEnabledExtensionNames = (const char *[]) {
+				VK_EXT_DEBUG_REPORT_EXTENSION_NAME,
+			},
+#endif
+		};
+		assert(vkCreateInstance(&info, NULL, &inst) == VK_SUCCESS);
+	}
+
+#ifdef ENABLE_DEBUG_LAYERS
+	{
+		typedef VkResult (*create_callback_func_t)(
+			VkInstance instance,
+			const VkDebugReportCallbackCreateInfoEXT* pCreateInfo,
+			const VkAllocationCallbacks* pAllocator,
+			VkDebugReportCallbackEXT* pCallback);
+
+		create_callback_func_t create_callback = (create_callback_func_t)
+			vkGetInstanceProcAddr(inst, "vkCreateDebugReportCallbackEXT");
+
+		VkDebugReportCallbackCreateInfoEXT info = {
+			.sType = VK_STRUCTURE_TYPE_DEBUG_REPORT_CREATE_INFO_EXT,
+			.flags = VK_DEBUG_REPORT_ERROR_BIT_EXT |
+			         VK_DEBUG_REPORT_WARNING_BIT_EXT |
+			         VK_DEBUG_REPORT_PERFORMANCE_WARNING_BIT_EXT,
+			.pfnCallback = debug_callback,
+		};
+		VkDebugReportCallbackEXT callback;
+		assert(create_callback(inst, &info, NULL, &callback) == VK_SUCCESS);
+	}
+#endif
+
+	VkPhysicalDevice phys;
+	{
+	        uint32_t physCount = 0;
+		assert(vkEnumeratePhysicalDevices(inst, &physCount, NULL) == VK_SUCCESS);
+
+		VkPhysicalDevice *devs = calloc(sizeof(*devs), physCount);
+		assert(vkEnumeratePhysicalDevices(inst, &physCount, devs) == VK_SUCCESS);
+
+		int i;
+		for (i = 0; i < physCount; i++) {
+			VkPhysicalDeviceProperties deviceProperties;
+			vkGetPhysicalDeviceProperties(devs[i], &deviceProperties);
+			VkPhysicalDeviceFeatures deviceFeatures;
+			vkGetPhysicalDeviceFeatures(devs[i], &deviceFeatures);
+
+			if (deviceProperties.deviceType == VK_PHYSICAL_DEVICE_TYPE_INTEGRATED_GPU ||
+			    deviceProperties.deviceType == VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU)
+				break;
+		}
+		assert(i < physCount);
+		phys = devs[i];
+		free(devs);
+	}
+
+	VkPhysicalDeviceMemoryProperties memoryProperties = {};
+	vkGetPhysicalDeviceMemoryProperties(phys, &memoryProperties);
+
+	uint32_t MemoryTypeIndex;
+	for (int i = 0; i < memoryProperties.memoryHeapCount; i++) {
+		if (memoryProperties.memoryTypes[i].propertyFlags &
+		    VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT) {
+			MemoryTypeIndex = i;
+			break;
+		}
+	}
+
+	VkDevice device;
+	{
+		const float zero = 0.0f;
+		VkDeviceQueueCreateInfo queueInfo = {
+			.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO,
+			.queueFamilyIndex = 0,
+			.queueCount = 1,
+			.pQueuePriorities = &zero,
+		};
+		VkDeviceCreateInfo info = {
+			.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO,
+			.queueCreateInfoCount = 1,
+			.pQueueCreateInfos = &queueInfo,
+		};
+		assert(vkCreateDevice(phys, &info, NULL, &device) == VK_SUCCESS);
+	}
+
+	VkQueue queue;
+	vkGetDeviceQueue(device, 0, 0, &queue);
+
+	VkCommandPool commandPool;
+	{
+		VkCommandPoolCreateInfo info = {
+			.sType = VK_STRUCTURE_TYPE_COMMAND_POOL_CREATE_INFO,
+			.flags = VK_COMMAND_POOL_CREATE_RESET_COMMAND_BUFFER_BIT,
+			.queueFamilyIndex = 0,
+		};
+		assert(vkCreateCommandPool(device, &info, NULL, &commandPool) == VK_SUCCESS);
+	}
+
+	VkCommandBuffer commandBuffer;
+	{
+		VkCommandBufferAllocateInfo info = {
+			.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO,
+			.commandPool = commandPool,
+			.commandBufferCount = 1,
+		};
+		assert(vkAllocateCommandBuffers(device, &info, &commandBuffer) == VK_SUCCESS);
+	}
+
+	VkRenderPass renderPass;
+	{
+		VkAttachmentDescription color = {
+			.format = VK_FORMAT_R8G8B8A8_UNORM,
+			.samples = VK_SAMPLE_COUNT_1_BIT,
+			.loadOp = VK_ATTACHMENT_LOAD_OP_CLEAR,
+			.storeOp = VK_ATTACHMENT_STORE_OP_STORE,
+			.initialLayout = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL,
+			.finalLayout = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL,
+		};
+		VkAttachmentReference colorRef = {
+			.attachment = 0,
+			.layout = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL,
+		};
+		VkSubpassDescription render = {
+			.colorAttachmentCount = 1,
+			.pColorAttachments = &colorRef,
+		};
+		VkRenderPassCreateInfo info = {
+			.sType = VK_STRUCTURE_TYPE_RENDER_PASS_CREATE_INFO,
+			.attachmentCount = 1,
+			.pAttachments = &color,
+			.subpassCount = 1,
+			.pSubpasses = &render,
+		};
+		assert(vkCreateRenderPass(device, &info, NULL, &renderPass) == VK_SUCCESS);
+	}
+
+	VkImage image;
+	{
+		VkImageCreateInfo info = {
+			.sType = VK_STRUCTURE_TYPE_IMAGE_CREATE_INFO,
+			.imageType = VK_IMAGE_TYPE_2D,
+			.format = VK_FORMAT_R8G8B8A8_UNORM,
+			.extent = {TARGET_W, TARGET_H, 1},
+			.mipLevels = 1,
+			.arrayLayers = 1,
+			.samples = VK_SAMPLE_COUNT_1_BIT,
+			.tiling = VK_IMAGE_TILING_LINEAR,
+			.usage = VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT,
+			.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED,
+		};
+		assert(vkCreateImage(device, &info, NULL, &image) == VK_SUCCESS);
+	}
+	VkDeviceMemory imageMemory;
+	{
+		VkMemoryRequirements requirements;
+		vkGetImageMemoryRequirements(device, image, &requirements);
+
+		VkMemoryAllocateInfo info = {
+			.sType = VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO,
+			.allocationSize = requirements.size,
+			.memoryTypeIndex = MemoryTypeIndex,
+		};
+		assert(vkAllocateMemory(device, &info, NULL, &imageMemory) == VK_SUCCESS);
+		assert(vkBindImageMemory(device, image, imageMemory, 0) == VK_SUCCESS);
+	}
+	VkImageView color;
+	{
+		VkImageViewCreateInfo info = {
+			.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO,
+			.image = image,
+			.viewType = VK_IMAGE_VIEW_TYPE_2D,
+			.format = VK_FORMAT_R8G8B8A8_UNORM,
+			.subresourceRange = {
+				.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT,
+				.baseMipLevel = 0,
+				.levelCount = 1,
+				.baseArrayLayer = 0,
+				.layerCount = 1,
+			},
+		};
+		assert(vkCreateImageView(device, &info, NULL, &color) == VK_SUCCESS);
+	}
+
+	float vertex[] = {
+		-1, -1,
+		-1,  1,
+		 1,  1,
+	};
+	VkBuffer buffer;
+	{
+		VkBufferCreateInfo info = {
+			.sType = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO,
+			.size = sizeof(vertex),
+			.usage = VK_BUFFER_USAGE_VERTEX_BUFFER_BIT,
+		};
+		assert(vkCreateBuffer(device, &info, NULL, &buffer) == VK_SUCCESS);
+	}
+	VkDeviceMemory bufferMemory;
+	{
+		VkMemoryRequirements requirements;
+		vkGetBufferMemoryRequirements(device, buffer, &requirements);
+
+		VkMemoryAllocateInfo info = {
+			.sType = VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO,
+			.allocationSize = requirements.size,
+			.memoryTypeIndex = MemoryTypeIndex,
+		};
+		assert(vkAllocateMemory(device, &info, NULL, &bufferMemory) == VK_SUCCESS);
+		assert(vkBindBufferMemory(device, buffer, bufferMemory, 0) == VK_SUCCESS);
+
+		void* data;
+		assert(vkMapMemory(device, bufferMemory, 0, VK_WHOLE_SIZE, 0, &data) == VK_SUCCESS);
+		memcpy(data, vertex, sizeof(vertex));
+		vkUnmapMemory(device, bufferMemory);
+	}
+
+	VkFramebuffer framebuffer;
+	{
+		VkFramebufferCreateInfo info = {
+			.sType = VK_STRUCTURE_TYPE_FRAMEBUFFER_CREATE_INFO,
+			.renderPass = renderPass,
+			.attachmentCount = 1,
+			.pAttachments = &color,
+			.width = TARGET_W,
+			.height = TARGET_H,
+			.layers = 1,
+		};
+		assert(vkCreateFramebuffer(device, &info, NULL, &framebuffer) == VK_SUCCESS);
+	}
+
+	VkShaderModule vs = createShaderModule(device, "vert.spv");
+	VkShaderModule fs = createShaderModule(device, "frag.spv");
+
+	VkPipelineLayout pipelineLayout;
+	{
+		VkPipelineLayoutCreateInfo info = {
+			.sType = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO,
+			.setLayoutCount = 0,
+			.pushConstantRangeCount = 0,
+		};
+		assert(vkCreatePipelineLayout(device, &info, NULL, &pipelineLayout) == VK_SUCCESS);
+	}
+
+	VkPipeline pipeline;
+	{
+		VkVertexInputBindingDescription binding = {
+			.binding = 0,
+			.stride = 2 * sizeof(float),
+		};
+
+		VkVertexInputAttributeDescription attribute = {
+			.location = 0,
+			.binding = binding.binding,
+			.format = VK_FORMAT_R32G32_SFLOAT,
+			.offset = 0,
+		};
+
+		VkPipelineVertexInputStateCreateInfo vertexInputInfo = {
+			.sType = VK_STRUCTURE_TYPE_PIPELINE_VERTEX_INPUT_STATE_CREATE_INFO,
+			.vertexBindingDescriptionCount = 1,
+			.pVertexBindingDescriptions = &binding,
+			.vertexAttributeDescriptionCount = 1,
+			.pVertexAttributeDescriptions = &attribute,
+		};
+
+		VkPipelineInputAssemblyStateCreateInfo inputAssemblyInfo = {
+			.sType = VK_STRUCTURE_TYPE_PIPELINE_INPUT_ASSEMBLY_STATE_CREATE_INFO,
+			.topology = VK_PRIMITIVE_TOPOLOGY_TRIANGLE_LIST,
+		};
+
+		VkViewport viewport = {
+			.width = TARGET_W,
+			.height = TARGET_H,
+			.maxDepth = 1.0f,
+		};
+
+		VkRect2D scissor = {
+			{0, 0}, {TARGET_W, TARGET_H}
+		};
+
+		VkPipelineViewportStateCreateInfo viewportInfo = {
+			.sType = VK_STRUCTURE_TYPE_PIPELINE_VIEWPORT_STATE_CREATE_INFO,
+			.viewportCount = 1,
+			.pViewports = &viewport,
+			.scissorCount = 1,
+			.pScissors = &scissor,
+		};
+
+		VkPipelineRasterizationStateCreateInfo rasterizationInfo = {
+			.sType = VK_STRUCTURE_TYPE_PIPELINE_RASTERIZATION_STATE_CREATE_INFO,
+			.lineWidth = 1.0f,
+		};
+
+		VkPipelineMultisampleStateCreateInfo multisampleInfo = {
+			.sType = VK_STRUCTURE_TYPE_PIPELINE_MULTISAMPLE_STATE_CREATE_INFO,
+			.rasterizationSamples = VK_SAMPLE_COUNT_1_BIT,
+		};
+
+		VkPipelineColorBlendAttachmentState blend = {
+			.colorWriteMask =
+			VK_COLOR_COMPONENT_R_BIT |
+			VK_COLOR_COMPONENT_G_BIT |
+			VK_COLOR_COMPONENT_B_BIT |
+			VK_COLOR_COMPONENT_A_BIT,
+		};
+		VkPipelineColorBlendStateCreateInfo colorBlendInfo = {
+			.sType = VK_STRUCTURE_TYPE_PIPELINE_COLOR_BLEND_STATE_CREATE_INFO,
+			.attachmentCount = 1,
+			.pAttachments = &blend,
+		};
+
+		VkPipelineShaderStageCreateInfo stages[2] = {
+			[0] = {
+				.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO,
+				.stage = VK_SHADER_STAGE_VERTEX_BIT,
+				.module = vs,
+				.pName = "main",
+			},
+			[1] = {
+				.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO,
+				.stage = VK_SHADER_STAGE_FRAGMENT_BIT,
+				.module = fs,
+				.pName = "main",
+			},
+		};
+
+		VkGraphicsPipelineCreateInfo info = {
+			.sType = VK_STRUCTURE_TYPE_GRAPHICS_PIPELINE_CREATE_INFO,
+			.stageCount = 2,
+			.pStages = stages,
+			.pVertexInputState = &vertexInputInfo,
+			.pInputAssemblyState = &inputAssemblyInfo,
+			.pViewportState = &viewportInfo,
+			.pRasterizationState = &rasterizationInfo,
+			.pMultisampleState = &multisampleInfo,
+			.pColorBlendState = &colorBlendInfo,
+			.layout = pipelineLayout,
+			.renderPass = renderPass,
+			.subpass = 0,
+		};
+		assert(vkCreateGraphicsPipelines(device, NULL, 1, &info, NULL, &pipeline) == VK_SUCCESS);
+	}
+
+	{
+		VkCommandBufferBeginInfo info = {
+			.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO,
+		};
+		assert(vkBeginCommandBuffer(commandBuffer, &info) == VK_SUCCESS);
+	}
+
+	{
+		VkImageMemoryBarrier barrier = {
+			.sType = VK_STRUCTURE_TYPE_IMAGE_MEMORY_BARRIER,
+			.dstAccessMask = VK_ACCESS_COLOR_ATTACHMENT_WRITE_BIT,
+			.oldLayout = VK_IMAGE_LAYOUT_UNDEFINED,
+			.newLayout = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL,
+			.srcQueueFamilyIndex = VK_QUEUE_FAMILY_IGNORED,
+			.dstQueueFamilyIndex = VK_QUEUE_FAMILY_IGNORED,
+			.image = image,
+			.subresourceRange = {
+				.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT,
+				.baseMipLevel = 0,
+				.levelCount = 1,
+				.baseArrayLayer = 0,
+				.layerCount = 1,
+			},
+		};
+		vkCmdPipelineBarrier(commandBuffer, VK_PIPELINE_STAGE_ALL_COMMANDS_BIT,
+				     VK_PIPELINE_STAGE_ALL_COMMANDS_BIT, 0, 0, NULL, 0,
+				     NULL, 1, &barrier);
+	}
+
+	{
+		VkClearValue color = {
+			.color = {
+				.float32 = {0},
+			},
+		};
+		VkRenderPassBeginInfo info = {
+			.sType = VK_STRUCTURE_TYPE_RENDER_PASS_BEGIN_INFO,
+			.renderPass = renderPass,
+			.framebuffer = framebuffer,
+			.renderArea = {{0, 0}, {TARGET_W, TARGET_H}},
+			.clearValueCount = 1,
+			.pClearValues = &color,
+		};
+		vkCmdBeginRenderPass(commandBuffer, &info, VK_SUBPASS_CONTENTS_INLINE);
+	}
+
+	vkCmdBindPipeline(commandBuffer, VK_PIPELINE_BIND_POINT_GRAPHICS, pipeline);
+
+	{
+		VkDeviceSize offset = 0;
+		vkCmdBindVertexBuffers(commandBuffer, 0, 1, &buffer, &offset);
+	}
+
+	vkCmdDraw(commandBuffer, 3, 1, 0, 0);
+
+	vkCmdEndRenderPass(commandBuffer);
+
+	assert(vkEndCommandBuffer(commandBuffer) == VK_SUCCESS);
+
+	VkFence fence;
+	{
+		VkFenceCreateInfo info = {
+			info.sType = VK_STRUCTURE_TYPE_FENCE_CREATE_INFO,
+		};
+		assert(vkCreateFence(device, &info, NULL, &fence) == VK_SUCCESS);
+	}
+
+	{
+		VkSubmitInfo info = {
+			.sType = VK_STRUCTURE_TYPE_SUBMIT_INFO,
+			.commandBufferCount = 1,
+			.pCommandBuffers = &commandBuffer,
+		};
+		assert(vkQueueSubmit(queue, 1, &info, fence) == VK_SUCCESS);
+	}
+
+	assert(vkWaitForFences(device, 1, &fence, 1, 1000000000ull) == VK_SUCCESS);
+
+	{
+		void* data;
+		assert(vkMapMemory(device, imageMemory, 0, VK_WHOLE_SIZE, 0, &data) == VK_SUCCESS);
+
+		assert(!writeImage("screenshot.png", TARGET_W, TARGET_H, TARGET_W * 4,
+				   data, "hello"));
+
+		vkUnmapMemory(device, imageMemory);
+	}
+
+	vkDestroyPipeline(device, pipeline, NULL);
+	vkDestroyPipelineLayout(device, pipelineLayout, NULL);
+	vkDestroyShaderModule(device, vs, NULL);
+	vkDestroyShaderModule(device, fs, NULL);
+	vkDestroyFramebuffer(device, framebuffer, NULL);
+	vkDestroyBuffer(device, buffer, NULL);
+	vkFreeMemory(device, bufferMemory, NULL);
+	vkDestroyImageView(device, color, NULL);
+	vkDestroyImage(device, image, NULL);
+	vkFreeMemory(device, imageMemory, NULL);
+	vkDestroyRenderPass(device, renderPass, NULL);
+	vkDestroyFence(device, fence, NULL);
+	vkFreeCommandBuffers(device, commandPool, 1, &commandBuffer);
+	vkDestroyCommandPool(device, commandPool, NULL);
+	vkDestroyDevice(device, NULL);
+	vkDestroyInstance(inst, NULL);
+}
